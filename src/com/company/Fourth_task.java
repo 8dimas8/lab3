c14c93666a922345beeb43cc6e256bc9803d8151
8dimas8 <dimikoff008@gmail.com>
2022-03-01 09:54:23 +0200

    new files

---
+++ src/com/company/Fourth_task.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class Fourth_task {
+    public static void main(String[] args){
+        String assertion="+";
+        switch(assertion) {
+            case "Так":
+                System.out.println("Я погоджуюсь!");
+                break;
+            case "OK":
+                System.out.println("Я погоджуюсь!");
+                break;
+            case "Yes":
+                System.out.println("Я погоджуюсь!");
+                break;
+            case "Y":
+                System.out.println("Я погоджуюсь!");
+                break;
+            case "+":
+                System.out.println("Я погоджуюсь!");
+                break;
+            case "Ok":
+                System.out.println("Я погоджуюсь!");
+                break;
+
+
+        }
+        switch (assertion) {
+            case "Ні":
+                System.out.println("Я не погоджуюсь!");
+                break;
+            case "NO":
+                System.out.println("Я не погоджуюсь!");
+                break;
+            case "N":
+                System.out.println("Я не погоджуюсь!");
+                break;
+            case "-":
+                System.out.println("Я не погоджуюсь!");
+                break;
+            case "No":
+                System.out.println("Я не погоджуюсь!");
+                break;
+
+        }
+    }
+}
